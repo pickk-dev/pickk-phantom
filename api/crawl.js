aeb72cd900d7958852d56b67a2a38bf106c24795
Choi Sumin <greatSumini@gmail.com>
2020-02-25 19:03:15 +0900

    [Feature] add color crawling

--- api/crawl.js
+++ api/crawl.js
@@ -1,13 +1,26 @@
-'use strict';
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-module.exports.request = (event, context, callback) => {
-  const response = {
+module.exports.request = async (event, context, callback) => {
+  // data 읽기
+  const { data: html } = await axios(event['queryStringParameters']['url']);
+  const $ = cheerio.load(html);
+
+  const search = $('#product_option_id1');
+  const colors = [];
+  search.children().each((i, e) => {
+    if (i >= 2) {
+      colors.push($(e).text());
+    }
+  });
+  console.log(colors);
+  const options_set = [];
+
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
+      data: colors
     })
-  };
-
-  callback(null, response);
+  });
 };
