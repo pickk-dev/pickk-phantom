165fcc48fd743ce11295583ce38886607658c32d
Choi Sumin <greatSumini@gmail.com>
2020-03-03 13:30:06 +0900

    [Enhance] refactor piece-workder crawler

--- models/crawler/piece-worker.ts
+++ models/crawler/piece-worker.ts
@@ -1,14 +1,22 @@
-import { getCafe24StockData } from '.';
+import axios from 'axios';
+
+import { getCafe24Data } from '.';
+import { ICrawler, evaluateData } from '../../types/ICrawler';
+import { formatData } from '../../lib/Cafe24Parser';
 
 declare const EC_SHOP_FRONT_NEW_OPTION_DATA;
 
-export default class PieceWorkerCrawler {
+export default class PieceWorkerCrawler implements ICrawler {
   url: string;
 
   evaluate = () => {
-    return EC_SHOP_FRONT_NEW_OPTION_DATA.aItemStockData[
-      Number(window.location.href.split('/')[5])
-    ];
+    return {
+      type: 'stock' as evaluateData,
+      data:
+        EC_SHOP_FRONT_NEW_OPTION_DATA.aItemStockData[
+          Number(window.location.href.split('/')[5])
+        ]
+    };
   };
 
   constructor(url: string) {
@@ -16,24 +24,9 @@ export default class PieceWorkerCrawler {
   }
 
   request = async () => {
-    const stockData = await getCafe24StockData(this.url, this.evaluate);
-    const option = {
-      name: '옵션',
-      items: [{ name: '사이즈', items: [] }]
-    };
-    console.log(stockData);
-    Object.values(stockData).forEach(
-      (value: { option_value_orginal: string[]; stock_number: number }) => {
-        const [size] = value.option_value_orginal;
-        if (value.stock_number === 0) {
-          return;
-        }
-        option.items[0].items.push({
-          value: size,
-          index: option.items[0].items.length
-        });
-      }
-    );
+    const optionNames = ['사이즈'];
+    const { type, data } = await getCafe24Data(this.url, this.evaluate);
+    const option = formatData(type, data, optionNames);
     return Promise.resolve(option);
   };
 }
