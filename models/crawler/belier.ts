4ec8bfb80640cde60d2fa205dcedb8980b12d862
gywls517 <gywls517@naver.com>
2020-03-08 17:01:37 +0900

    [Feature] add belier crawler

---
+++ models/crawler/belier.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+import { getCafe24Data } from ".";
+import { ICrawler, evaluateData } from "../../types/ICrawler";
+import { formatData } from "../../lib/Cafe24Parser";
+import { getProductNum } from "../../lib/URLparser";
+
+declare const EC_SHOP_FRONT_NEW_OPTION_DATA;
+
+export default class BelierCrawler implements ICrawler {
+  url: string;
+  productNum: number;
+  itemIsSoldOut: boolean;
+
+  evaluate = (productNum: number) => {
+    return {
+      type: "stock" as evaluateData,
+      data: EC_SHOP_FRONT_NEW_OPTION_DATA.aItemStockData[productNum]
+    };
+  };
+
+  getOptionNames = async () => {
+    const optionNames = [];
+    const { data: body } = await axios(this.url);
+    const hi = cheerio.load(body);
+    hi("#product_option_id1 > optgroup").each((_, ele) => {
+      optionNames.push(ele.attribs.label);
+    });
+    if (optionNames.length === 0) this.getItemIsSoldOut(hi);
+    return Promise.resolve(optionNames);
+  };
+
+  getItemIsSoldOut = (hi: CheerioStatic) => {
+    hi(
+      "div.xans-element-.xans-product.xans-product-action.detail_button > .displaynone > span"
+    ).each((_, ele) => {
+      if (ele.children[0].data === "SOLD OUT") this.itemIsSoldOut = false;
+    });
+  };
+
+  constructor(url: string) {
+    this.url = url;
+    this.productNum = getProductNum(url);
+    this.itemIsSoldOut = true;
+  }
+
+  request = async () => {
+    const optionNames = await this.getOptionNames();
+    const { type, data } = await getCafe24Data(
+      this.url,
+      this.evaluate,
+      this.productNum
+    );
+    const option =
+      data === undefined
+        ? formatData(type, this.itemIsSoldOut, optionNames)
+        : formatData(type, data, optionNames);
+    return Promise.resolve(option);
+  };
+}
