d505ac1dec88316c37d8ef7b275b07ce2e3dc9c7
gywls517 <gywls517@naver.com>
2020-03-08 17:01:55 +0900

    [Feature] add lohnt crawler

---
+++ models/crawler/lohnt.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+import { getCafe24Data } from ".";
+import { ICrawler, evaluateData, evaluateResponse } from "../../types/ICrawler";
+import { formatData } from "../../lib/Cafe24Parser";
+import { getProductNum } from "../../lib/URLparser";
+
+declare const EC_SHOP_FRONT_NEW_OPTION_DATA;
+
+export default class LohntCrawler implements ICrawler {
+  url: string;
+  productNum: number;
+  itemIsSoldOut: boolean;
+
+  evaluate = (productNum: number): evaluateResponse => {
+    return {
+      type: "stock" as evaluateData,
+      data: EC_SHOP_FRONT_NEW_OPTION_DATA.aItemStockData[productNum]
+    };
+  };
+
+  getOptionNames = async () => {
+    const optionNames = [];
+    const { data: body } = await axios(this.url);
+    const hi = cheerio.load(body);
+    hi(
+      "table > tbody.xans-element-.xans-product.xans-product-option.xans-record- > tr > th"
+    ).each((_, ele) => {
+      optionNames.push(ele.children[0].data);
+    });
+    if (optionNames.length === 0) this.getItemIsSoldOut(hi);
+    return Promise.resolve(optionNames);
+  };
+
+  getItemIsSoldOut = (hi: CheerioStatic) => {
+    let itemIsSoldOut = true;
+    hi("div.btnArea > .displaynone > p").each((_, ele) => {
+      if (ele.children[0].data === "SOLD OUT") itemIsSoldOut = false;
+    });
+    this.itemIsSoldOut = itemIsSoldOut;
+  };
+
+  constructor(url: string) {
+    this.url = url;
+    this.productNum = getProductNum(url);
+  }
+
+  request = async () => {
+    const optionNames = await this.getOptionNames();
+    const { type, data } = await getCafe24Data(
+      this.url,
+      this.evaluate,
+      this.productNum
+    );
+    const option =
+      data === undefined
+        ? formatData(type, this.itemIsSoldOut, optionNames)
+        : formatData(type, data, optionNames);
+    return Promise.resolve(option);
+  };
+}
