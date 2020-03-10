1a7eb67775b3512236a4bac1b4a1e6142a07602c
gywls517 <gywls517@naver.com>
2020-03-10 15:26:21 +0900

    [Enhance] Add the code which sets productPriceVariants

--- lib/Cafe24Parser.ts
+++ lib/Cafe24Parser.ts
@@ -1,4 +1,9 @@
-import { evaluateData, stockData, optionDefaultData } from "../types/ICrawler";
+import {
+  evaluateData,
+  stockData,
+  optionDefaultData,
+  itemOptionData
+} from "../types/ICrawler";
 import * as cheerio from "cheerio";
 
 export const formatData = (type: evaluateData, data, optionNames: string[]) => {
@@ -11,10 +16,11 @@ const formatOptionDefaultData = (
   data: optionDefaultData,
   optionNames: string[]
 ) => {
-  const option = {
+  const option: itemOptionData = {
     values: {},
     isSoldOut: [],
-    optionPriceVariants: []
+    optionPriceVariants: [],
+    productPriceVariants: []
   };
 
   const optionPriceVariants = JSON.parse(data.optionPriceVariants);
@@ -39,6 +45,35 @@ const formatOptionDefaultData = (
       });
   });
 
+  const optionValuesArr = Object.values(option.values);
+  const divisorArr = optionValuesArr.map(e => e.length).reverse();
+  let mul = 1;
+  divisorArr.forEach(e => (mul *= e));
+  const optionVariants = Array.apply(null, Array(mul)).map((_, index) => {
+    let bf = index;
+    let optionVariant = [];
+    divisorArr.forEach(divisor => {
+      optionVariant.unshift(Math.floor(bf % divisor));
+      bf = Math.floor(bf / divisor);
+    });
+    return optionVariant;
+  });
+
+  optionVariants.forEach(optionVariant => {
+    const totalPrice = option.optionPriceVariants.reduce(
+      (acc, optionPriceVariant) => {
+        const { option, price } = optionPriceVariant;
+        return optionVariant[option[0]] === option[1] ? acc + price : acc;
+      },
+      0
+    );
+    if (totalPrice > 0)
+      option.productPriceVariants.push({
+        option: optionVariant,
+        price: totalPrice
+      });
+  });
+
   return option;
 };
 
@@ -46,7 +81,8 @@ const formatStockData = (data: stockData | boolean, optionNames: string[]) => {
   const option = {
     values: {},
     isSoldOut: [],
-    optionPriceVariants: []
+    optionPriceVariants: [],
+    productPriceVariants: []
   };
 
   if (optionNames.length !== 0)
