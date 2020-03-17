5646e30d40e5c2db13355c09e48e284d745a0a16
Choi Sumin <greatSumini@gmail.com>
2020-03-17 19:02:06 +0900

    [Bugfix] fix unescaped path error - 한글이 문제였음. 받은 url을 encode해서 해결

--- models/crawler/index.ts
+++ models/crawler/index.ts
@@ -1,68 +1,70 @@
-import * as phantom from "phantom";
+import * as phantom from 'phantom';
 
-import { ICrawler, evaluateResponse } from "../../types/ICrawler";
-import TheKnitCompanyCrawler from "./the-knit-company";
-import FatalismCrawler from "./fatalism";
-import PieceWorkerCrawler from "./piece-worker";
-import GarmentLableCrawler from "./garment-lable";
-import BananafitCrawler from "./bananafit";
-import EightySixRoadCrawler from "./eightysix-road";
-import LohntCrawler from "./lohnt";
-import BelierCrawler from "./belier";
-import RomanticMoveCrawler from "./romanticmove";
-import NotnnotCrawler from "./notnnot";
-import CostumeoclockCrawler from "./costumeoclock";
-import SuareCrawler from "./suare";
-import HanceCrawler from "./hance";
+import { ICrawler, evaluateResponse } from '../../types/ICrawler';
+import TheKnitCompanyCrawler from './the-knit-company';
+import FatalismCrawler from './fatalism';
+import PieceWorkerCrawler from './piece-worker';
+import GarmentLableCrawler from './garment-lable';
+import BananafitCrawler from './bananafit';
+import EightySixRoadCrawler from './eightysix-road';
+import LohntCrawler from './lohnt';
+import BelierCrawler from './belier';
+import RomanticMoveCrawler from './romanticmove';
+import NotnnotCrawler from './notnnot';
+import CostumeoclockCrawler from './costumeoclock';
+import SuareCrawler from './suare';
+import HanceCrawler from './hance';
 
 export const getCrawler = (url: string): ICrawler => {
-  const { origin } = new URL(url);
+  const uniCodeUrl = encodeURI(url);
 
-  if (origin === "https://theknitcompany.com")
-    return new TheKnitCompanyCrawler(url);
-  if (origin === "https://garment-lable.com")
-    return new GarmentLableCrawler(url);
-  if (origin === "http://86road.co.kr" || origin === "https://86road.co.kr")
-    return new EightySixRoadCrawler(url);
-  if (origin === "http://fatalism.co.kr" || origin === "https://fatalism.co.kr")
-    return new FatalismCrawler(url);
+  const { origin } = new URL(uniCodeUrl);
+
+  if (origin === 'https://theknitcompany.com')
+    return new TheKnitCompanyCrawler(uniCodeUrl);
+  if (origin === 'https://garment-lable.com')
+    return new GarmentLableCrawler(uniCodeUrl);
+  if (origin === 'http://86road.co.kr' || origin === 'https://86road.co.kr')
+    return new EightySixRoadCrawler(uniCodeUrl);
+  if (origin === 'http://fatalism.co.kr' || origin === 'https://fatalism.co.kr')
+    return new FatalismCrawler(uniCodeUrl);
   if (
-    origin === "http://piece-worker.com" ||
-    origin === "https://piece-worker.com"
+    origin === 'http://piece-worker.com' ||
+    origin === 'https://piece-worker.com'
   )
-    return new PieceWorkerCrawler(url);
+    return new PieceWorkerCrawler(uniCodeUrl);
   if (
-    origin === "http://bananafit.co.kr" ||
-    origin === "https://bananafit.co.kr"
+    origin === 'http://bananafit.co.kr' ||
+    origin === 'https://bananafit.co.kr'
   )
-    return new BananafitCrawler(url);
+    return new BananafitCrawler(uniCodeUrl);
 
   if (
-    origin === "http://www.belier.co.kr" ||
-    origin === "https://www.belier.co.kr"
+    origin === 'http://www.belier.co.kr' ||
+    origin === 'https://www.belier.co.kr'
   )
-    return new BelierCrawler(url);
-  if (origin === "http://lohnt.co.kr" || origin === "https://lohnt.co.kr")
-    return new LohntCrawler(url);
+    return new BelierCrawler(uniCodeUrl);
+  if (origin === 'http://lohnt.co.kr' || origin === 'https://lohnt.co.kr')
+    return new LohntCrawler(uniCodeUrl);
   if (
-    origin === "http://romanticmove.com" ||
-    origin === "https://romanticmove.com"
+    origin === 'http://romanticmove.com' ||
+    origin === 'https://romanticmove.com'
   )
-    return new RomanticMoveCrawler(url);
-  if (origin === "http://notnnot.com" || origin === "https://notnnot.com") {
-    return new NotnnotCrawler(url);
+    return new RomanticMoveCrawler(uniCodeUrl);
+  if (origin === 'http://notnnot.com' || origin === 'https://notnnot.com') {
+    return new NotnnotCrawler(uniCodeUrl);
   }
   if (
-    origin === "http://costumeoclock.com" ||
-    origin === "https://costumeoclock.com"
+    origin === 'http://costumeoclock.com' ||
+    origin === 'https://costumeoclock.com'
   ) {
-    return new CostumeoclockCrawler(url);
+    return new CostumeoclockCrawler(uniCodeUrl);
   }
-  if (origin === "http://suare.co.kr" || origin === "https://suare.co.kr") {
-    return new SuareCrawler(url);
+  if (origin === 'http://suare.co.kr' || origin === 'https://suare.co.kr') {
+    return new SuareCrawler(uniCodeUrl);
   }
-  if (origin === "https://hance.kr") {
-    return new HanceCrawler(url);
+  if (origin === 'https://hance.kr') {
+    return new HanceCrawler(uniCodeUrl);
   }
 };
 
