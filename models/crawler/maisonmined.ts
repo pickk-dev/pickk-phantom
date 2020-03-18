550be4938b41e0719516e331e014a12ec15bf477
gywls517 <gywls517@naver.com>
2020-03-18 19:24:21 +0900

    [Feature] implement maison-mined crawling model

---
+++ models/crawler/maisonmined.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+import { getCafe24Data } from ".";
+import { ICrawler, evaluateData, evaluateResponse } from "../../types/ICrawler";
+import { formatData } from "../../lib/Cafe24Parser";
+import { getProductNum } from "../../lib/URLparser";
+
+declare const EC_SHOP_FRONT_NEW_OPTION_DATA;
+
+export default class MaisonMinedCrawler implements ICrawler {
+  url: string;
+  productNum: number;
+
+  evaluate = (productNum: number): evaluateResponse => {
+    return {
+      type: "stock" as evaluateData,
+      data: EC_SHOP_FRONT_NEW_OPTION_DATA.aItemStockData[productNum]
+    };
+  };
+
+  getOptionNames = async () => {
+    const optionNames = [];
+    const { data: body } = await axios(this.url);
+    const hi = cheerio.load(body);
+    hi(
+      "table > tbody.xans-element-.xans-product.xans-product-option.xans-record- > tr > td > p:nth-child(1)"
+    ).each((_, ele) => {
+      optionNames.push(...ele.children[0].data.split("-"));
+    });
+    return Promise.resolve(optionNames);
+  };
+
+  constructor(url: string) {
+    this.url = url;
+    this.productNum = getProductNum(url);
+  }
+
+  request = async () => {
+    const optionNames = await this.getOptionNames();
+    const { type, data } = await getCafe24Data(
+      this.url,
+      this.evaluate,
+      this.productNum
+    );
+    const option = formatData(type, data, optionNames);
+    return Promise.resolve(option);
+  };
+}
