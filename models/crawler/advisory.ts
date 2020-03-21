e8ede3d255485a2a3d518751d0c7e61f71a7aecb
gywls517 <gywls517@naver.com>
2020-03-21 12:39:58 +0900

    [Feature] Add Advisory crawling model

---
+++ models/crawler/advisory.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+import { ICrawler } from "../../types/ICrawler";
+import { getProductNum } from "../../lib/URLparser";
+
+export default class AdvisoryCrawler implements ICrawler {
+  url: string;
+  productNum: number;
+
+  getOptions = async () => {
+    const values = {};
+    const { data: body } = await axios(this.url);
+    const hi = cheerio.load(body);
+    const { optionValues, isSoldOut } = this.getOptionValues(hi);
+    values[this.getOptionName(hi)] = optionValues;
+    return Promise.resolve({ values, isSoldOut });
+  };
+
+  getOptionName = (hi: CheerioStatic) => {
+    let optionName;
+    hi("div.item_add_option_box > dl > dt").each((_, ele) => {
+      optionName = ele.children[0].data;
+    });
+    return optionName;
+  };
+
+  getOptionValues = (hi: CheerioStatic) => {
+    const optionValues = [];
+    const isSoldOut = [];
+    hi("div.item_add_option_box > dl > dd > select > option").each((i, ele) => {
+      if (ele.attribs.disabled) isSoldOut.push([i - 1]);
+      if (ele.attribs.value)
+        optionValues.push(ele.children[0].data.split("[품절]")[0].trim());
+    });
+    return { optionValues, isSoldOut };
+  };
+
+  constructor(url: string) {
+    this.url = url;
+    this.productNum = getProductNum(url);
+  }
+
+  request = async () => {
+    const option = await this.getOptions();
+    option["optionPriceVariants"] = [];
+    option["productPriceVariants"] = [];
+    return Promise.resolve(option);
+  };
+}
