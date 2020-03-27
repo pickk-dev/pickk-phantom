d212e426606bed8cf6a67538492666e435abc8ec
greatSumini <greatsumini@gmail.com>
2020-03-28 00:58:54 +0900

    [Enhance] add phantom-pool

--- models/crawler/index.ts
+++ models/crawler/index.ts
@@ -1,84 +1,89 @@
-import * as phantom from "phantom";
+import createPhantomPool from 'phantom-pool';
 
-import { ICrawler, evaluateResponse } from "../../types/ICrawler";
-import TheKnitCompanyCrawler from "./the-knit-company";
-import FatalismCrawler from "./fatalism";
-import PieceWorkerCrawler from "./piece-worker";
-import GarmentLableCrawler from "./garment-lable";
-import BananafitCrawler from "./bananafit";
-import EightySixRoadCrawler from "./eightysix-road";
-import LohntCrawler from "./lohnt";
-import BelierCrawler from "./belier";
-import RomanticMoveCrawler from "./romanticmove";
-import NotnnotCrawler from "./notnnot";
-import CostumeoclockCrawler from "./costumeoclock";
-import SuareCrawler from "./suare";
-import HanceCrawler from "./hance";
-import MaisonMinedCrawler from "./maisonmined";
-import AdvisoryCrawler from "./advisory";
-import ModnineCrawler from "./modnine";
+import { ICrawler, evaluateResponse } from '../../types/ICrawler';
+import TheKnitCompanyCrawler from './the-knit-company';
+import FatalismCrawler from './fatalism';
+import PieceWorkerCrawler from './piece-worker';
+import GarmentLableCrawler from './garment-lable';
+import BananafitCrawler from './bananafit';
+import EightySixRoadCrawler from './eightysix-road';
+import LohntCrawler from './lohnt';
+import BelierCrawler from './belier';
+import RomanticMoveCrawler from './romanticmove';
+import NotnnotCrawler from './notnnot';
+import CostumeoclockCrawler from './costumeoclock';
+import SuareCrawler from './suare';
+import HanceCrawler from './hance';
+import MaisonMinedCrawler from './maisonmined';
+import AdvisoryCrawler from './advisory';
+import ModnineCrawler from './modnine';
+
+const pool = createPhantomPool({
+  // For all opts, see opts at https://github.com/coopernurse/node-pool#createpool
+  phantomArgs: [['--load-images=true']] // arguments passed to phantomjs-node directly, default is `[]`. For all opts, see https://github.com/amir20/phantomjs-node#phantom-object-api
+});
 
 export const getCrawler = (url: string): ICrawler => {
   const uniCodeUrl = encodeURI(url);
 
   const { origin } = new URL(uniCodeUrl);
 
-  if (origin === "https://theknitcompany.com")
+  if (origin === 'https://theknitcompany.com')
     return new TheKnitCompanyCrawler(uniCodeUrl);
-  if (origin === "https://garment-lable.com")
+  if (origin === 'https://garment-lable.com')
     return new GarmentLableCrawler(uniCodeUrl);
-  if (origin === "http://86road.co.kr" || origin === "https://86road.co.kr")
+  if (origin === 'http://86road.co.kr' || origin === 'https://86road.co.kr')
     return new EightySixRoadCrawler(uniCodeUrl);
-  if (origin === "http://fatalism.co.kr" || origin === "https://fatalism.co.kr")
+  if (origin === 'http://fatalism.co.kr' || origin === 'https://fatalism.co.kr')
     return new FatalismCrawler(uniCodeUrl);
   if (
-    origin === "http://piece-worker.com" ||
-    origin === "https://piece-worker.com"
+    origin === 'http://piece-worker.com' ||
+    origin === 'https://piece-worker.com'
   )
     return new PieceWorkerCrawler(uniCodeUrl);
   if (
-    origin === "http://bananafit.co.kr" ||
-    origin === "https://bananafit.co.kr"
+    origin === 'http://bananafit.co.kr' ||
+    origin === 'https://bananafit.co.kr'
   )
     return new BananafitCrawler(uniCodeUrl);
 
   if (
-    origin === "http://www.belier.co.kr" ||
-    origin === "https://www.belier.co.kr"
+    origin === 'http://www.belier.co.kr' ||
+    origin === 'https://www.belier.co.kr'
   )
     return new BelierCrawler(uniCodeUrl);
-  if (origin === "http://lohnt.co.kr" || origin === "https://lohnt.co.kr")
+  if (origin === 'http://lohnt.co.kr' || origin === 'https://lohnt.co.kr')
     return new LohntCrawler(uniCodeUrl);
   if (
-    origin === "http://romanticmove.com" ||
-    origin === "https://romanticmove.com"
+    origin === 'http://romanticmove.com' ||
+    origin === 'https://romanticmove.com'
   )
     return new RomanticMoveCrawler(uniCodeUrl);
-  if (origin === "http://notnnot.com" || origin === "https://notnnot.com") {
+  if (origin === 'http://notnnot.com' || origin === 'https://notnnot.com') {
     return new NotnnotCrawler(uniCodeUrl);
   }
   if (
-    origin === "http://costumeoclock.com" ||
-    origin === "https://costumeoclock.com"
+    origin === 'http://costumeoclock.com' ||
+    origin === 'https://costumeoclock.com'
   ) {
     return new CostumeoclockCrawler(uniCodeUrl);
   }
-  if (origin === "http://suare.co.kr" || origin === "https://suare.co.kr") {
+  if (origin === 'http://suare.co.kr' || origin === 'https://suare.co.kr') {
     return new SuareCrawler(uniCodeUrl);
   }
-  if (origin === "https://hance.kr") {
+  if (origin === 'https://hance.kr') {
     return new HanceCrawler(uniCodeUrl);
   }
   if (
-    origin === "http://maison-mined.com" ||
-    origin === "https://maison-mined.com"
+    origin === 'http://maison-mined.com' ||
+    origin === 'https://maison-mined.com'
   ) {
     return new MaisonMinedCrawler(url);
   }
-  if (origin === "http://advisory.co.kr") {
+  if (origin === 'http://advisory.co.kr') {
     return new AdvisoryCrawler(url);
   }
-  if (origin === "http://modnine.com" || origin === "https://modnine.com") {
+  if (origin === 'http://modnine.com' || origin === 'https://modnine.com') {
     return new ModnineCrawler(url);
   }
 };
@@ -88,9 +93,13 @@ export const getCafe24Data = async (
   evaluate,
   productNum
 ): Promise<evaluateResponse> => {
-  const instance = await phantom.create();
-  const page = await instance.createPage();
-  const status = await page.open(url);
-  const data = await page.evaluate(evaluate, productNum);
+  const data = await pool.use(async instance => {
+    const page = await instance.createPage();
+    await page.clearCookies();
+    await page.open(url);
+    const result = await page.evaluate(evaluate, productNum);
+    await page.close();
+    return Promise.resolve(result as evaluateResponse);
+  });
   return Promise.resolve(data as evaluateResponse);
 };
