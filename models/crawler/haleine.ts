f83385ab28e17c482d08346d64692640e8647091
박효진 <47051596+gywlsp@users.noreply.github.com>
2020-04-27 19:30:27 +0900

    [Feature] Add haleine crawling model (#21)
    * [Feature] Add haleine crawling model
    
    * [Enhance] Add haleine crawling model to index crawler

---
+++ models/crawler/haleine.ts
@@ -0,0 +1,63 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+import { getCafe24Data } from ".";
+import { ICrawler, evaluateData, evaluateResponse } from "../../types/ICrawler";
+import { formatData } from "../../lib/Cafe24Parser";
+import { getProductNum } from "../../lib/URLparser";
+
+declare const EC_SHOP_FRONT_NEW_OPTION_DATA;
+
+export default class HaleineCrawler implements ICrawler {
+  url: string;
+  productNum: number;
+  itemIsSoldOut: boolean;
+
+  evaluate = (productNum: number): evaluateResponse => {
+    return {
+      type: "stock" as evaluateData,
+      data: EC_SHOP_FRONT_NEW_OPTION_DATA.aItemStockData[productNum],
+    };
+  };
+
+  getOptionNames = async () => {
+    const optionNames = [];
+    const { data: body } = await axios(this.url);
+    const hi = cheerio.load(body);
+    hi(
+      "table.xans-element-.xans-product.xans-product-option.xans-record- > tbody > tr > th.textHolderS"
+    ).each((_, ele) => {
+      optionNames.push(ele.children[0].data);
+    });
+    if (optionNames.length === 0) this.setItemIsSoldOut(hi);
+    return Promise.resolve(optionNames);
+  };
+
+  setItemIsSoldOut = (hi: CheerioStatic) => {
+    hi(
+      "div.xans-element-.xans-product.xans-product-action > div.ec-base-button > span.btn.displaynone"
+    ).each((_, ele) => {
+      if (ele.children[0].data === "SOLD OUT") this.itemIsSoldOut = false;
+    });
+  };
+
+  constructor(url: string) {
+    this.url = url;
+    this.productNum = getProductNum(url);
+    this.itemIsSoldOut = true;
+  }
+
+  request = async () => {
+    const optionNames = await this.getOptionNames();
+    const { type, data } = await getCafe24Data(
+      this.url,
+      this.evaluate,
+      this.productNum
+    );
+    const option =
+      data === undefined
+        ? formatData(type, this.itemIsSoldOut, optionNames)
+        : formatData(type, data, optionNames);
+    return Promise.resolve(option);
+  };
+}
