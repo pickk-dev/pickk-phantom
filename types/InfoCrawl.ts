24411efaeea70b795944cd23b41ab8c3c83a2688
greatSumini <greatsumini@gmail.com>
2020-05-04 19:37:01 +0900

    [Feature] add types/InfoCrawl - InfoCrawlResult, Selecters

---
+++ types/InfoCrawl.ts
@@ -0,0 +1,15 @@
+export type InfoCrawlResult = {
+  name: string;
+  brandKor: string;
+  imageUrl: string;
+  originalPrice: number;
+  salePrice: number;
+};
+
+export type Selecters = {
+  name: string;
+  brandKor: string;
+  imageUrl: string;
+  originalPrice: string;
+  salePrice: string;
+};
