b8ae7317140f2c312a16d1e6d0b1badb110a2b3e
greatSumini <greatsumini@gmail.com>
2020-07-17 11:37:07 +0900

    [Enhance] add oohahh.requestHtml

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,38 @@ export * from './Cafe24Parser';
 export * from './Phantom';
 export * from './URLparser';
 export * from './InfoCrawler';
+
+import request from 'request';
+import charset from 'charset'; // 해당 사이트의 charset값을 알 수 있게 해준다.
+import { decode } from 'iconv-lite';
+import axios from 'axios';
+
+export const requestHtml = async (sourceUrl: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: sourceUrl, // 원하는 url값을 입력
+        encoding: null, // 해당 값을 null로 해주어야 제대로 iconv가 제대로 decode 해준다.
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (compatible; Yeti/1.1; +http://naver.me/spd)', //            ''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+        },
+      },
+      async (error, res, body) => {
+        if (!error && res.statusCode === 200 && body.length > 1000) {
+          const enc = charset(res.headers, body); // 해당 사이트의 charset값을 획득
+          const iResult = decode(body, enc || 'utf-8'); // 획득한 charset값으로 body를 디코딩
+          resolve(iResult);
+        } else {
+          const { data } = await axios.get(sourceUrl, {
+            headers: {
+              'User-Agent':
+                'Mozilla/5.0 (compatible; Yeti/1.1; +http://naver.me/spd)',
+            },
+          });
+          resolve(data);
+        }
+      }
+    );
+  });
+};
--- models/crawler/oohahh.ts
+++ models/crawler/oohahh.ts
@@ -1,8 +1,7 @@
-import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 import { ICrawler, evaluateData, evaluateResponse } from '../../types/Crawl';
-import { getProductNum } from '../../lib/URLparser';
+import { requestHtml, getProductNum } from '../../lib';
 
 declare const EC_SHOP_FRONT_NEW_OPTION_DATA;
 
@@ -49,12 +48,7 @@ export default class OohAhhCrawler implements ICrawler {
   }
 
   request = async () => {
-    const { data: body } = await axios(this.url, {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
-      },
-    });
+    const body = await requestHtml(this.url);
     const hi = cheerio.load(body);
 
     const itemIsSoldOut = hi('body').html().includes('is_in_stock&quot;:false');
