916b24e477f39b8739dc5b6db1fba4a01d0518f6
greatSumini <greatsumini@gmail.com>
2020-08-11 16:51:13 +0900

    fix: :bug: add custumeoclock/mongdol crash handling logic

--- models/crawler/mongdol.ts
+++ models/crawler/mongdol.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
-import * as cheerio from "cheerio";
+import * as cheerio from 'cheerio';
 
-import { getCafe24Data } from ".";
-import { ICrawler, evaluateData, evaluateResponse } from "../../types/Crawl";
-import { formatData } from "../../lib/Cafe24Parser";
-import { getProductNum } from "../../lib/URLparser";
+import { getCafe24Data } from '.';
+import { ICrawler, evaluateData, evaluateResponse } from '../../types/Crawl';
+import { formatData } from '../../lib/Cafe24Parser';
+import { getProductNum } from '../../lib/URLparser';
+import { requestHtml } from 'lib';
 
 declare const EC_SHOP_FRONT_NEW_OPTION_DATA;
 
@@ -14,17 +14,17 @@ export default class MongdolCrawler implements ICrawler {
 
   evaluate = (productNum: number): evaluateResponse => {
     return {
-      type: "stock" as evaluateData,
+      type: 'stock' as evaluateData,
       data: EC_SHOP_FRONT_NEW_OPTION_DATA.aItemStockData[productNum],
     };
   };
 
   getOptionNames = async () => {
     const optionNames = [];
-    const { data: body } = await axios(this.url);
+    const body = await requestHtml(this.url);
     const hi = cheerio.load(body);
     hi(
-      "#contents > div.container2 > div.item2 > div > div.infoArea > table > tbody.xans-element-.xans-product.xans-product-option.xans-record- > tr > th"
+      '#contents > div.container2 > div.item2 > div > div.infoArea > table > tbody.xans-element-.xans-product.xans-product-option.xans-record- > tr > th'
     ).each((_, ele) => {
       optionNames.push(ele.children[0].data);
     });
