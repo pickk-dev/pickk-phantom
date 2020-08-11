c4d1fac6d10b81242973ab0ff83653ba5f84ff53
greatSumini <greatsumini@gmail.com>
2020-08-11 16:57:50 +0900

    fix: :bug: add costumeoclock crawl request error handle logic
    - 에러시 몽돌 크롤러 호출

--- controllers/Crawl.ts
+++ controllers/Crawl.ts
@@ -1,12 +1,20 @@
 import { getCrawler } from '../models/crawler';
+import MongdolCrawler from 'models/crawler/mongdol';
 
 const request = (url: string) => {
   const crawler = getCrawler(url);
-  return crawler.request();
+  try {
+    return crawler.request();
+  } catch (err) {
+    if (url.includes('costumeoclock')) {
+      return new MongdolCrawler(encodeURI(url)).request();
+    }
+    throw err;
+  }
 };
 
 const CrawlerController = {
-  request
+  request,
 };
 
 export default CrawlerController;
