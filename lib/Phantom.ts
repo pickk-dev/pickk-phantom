e3cb0823af3745fe7e57aec91a823cde28a720a1
greatSumini <greatsumini@gmail.com>
2020-08-31 11:28:20 +0900

    refactor: :boom: fix phantompool size (20 -> 5)

--- lib/Phantom.ts
+++ lib/Phantom.ts
@@ -1,7 +1,7 @@
 import createPhantomPool from 'phantom-pool';
 
 export const pool = createPhantomPool({
-  max: 20,
+  max: 5,
   // For all opts, see opts at https://github.com/coopernurse/node-pool#createpool
   phantomArgs: [['--load-images=true']], // arguments passed to phantomjs-node directly, default is `[]`. For all opts, see https://github.com/amir20/phantomjs-node#phantom-object-api
 });
